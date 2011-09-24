6460d25cd56c5be5427da63e830783226082bcdf
LiuQun <liuqun68@gmail.com>
2011-09-25 04:19:15 +0800

    完成图片显示测试

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
 #include <QApplication>
-#include <QMainWindow>
-
+#include <QPixmap>
+#include <QLabel>
+#include <QHBoxLayout>
 
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
 
-    QMainWindow window;
+    QWidget window;
+    QHBoxLayout *layout;
+    QLabel *label;
+    QLabel *logo;
+    QPixmap file;
+
+    label = new QLabel("Logo:");
+    label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
+    logo = new QLabel();
+    file.load(":/images/qt-logo.png");
+    logo->setPixmap(file);
+    layout = new QHBoxLayout();
+    layout->addWidget(label);
+    layout->addWidget(logo);
+    window.setLayout(layout);
     window.show();
 
     return app.exec();
