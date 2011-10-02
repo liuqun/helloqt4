a0f4fb3566629edf07f38c0fd64f763e7646062a
刘群 <liuqun68@gmail.com>
2011-10-02 17:52:08 +0800

    避免指针操作，以防忘记释放内存

--- main.cpp
+++ main.cpp
@@ -8,20 +8,18 @@ int main(int argc, char* argv[])
     QApplication app(argc, argv);
 
     QWidget window;
-    QHBoxLayout *layout;
-    QLabel *label;
-    QLabel *logo;
+    QHBoxLayout layout;
+    QLabel label;
+    QLabel logo;
     QPixmap file;
 
-    label = new QLabel("Logo:");
-    label->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
-    logo = new QLabel();
+    label.setText("Logo:");
+    label.setAlignment(Qt::AlignRight | Qt::AlignVCenter);
     file.load(":/images/qt-logo.png");
-    logo->setPixmap(file);
-    layout = new QHBoxLayout();
-    layout->addWidget(label);
-    layout->addWidget(logo);
-    window.setLayout(layout);
+    logo.setPixmap(file);
+    layout.addWidget(&label);
+    layout.addWidget(&logo);
+    window.setLayout(&layout);
     window.show();
 
     return app.exec();
